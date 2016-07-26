a3f837898fef3715cb97189fd9663c34336b8c93
Chris Grandin <cgrandin@shaw.ca>
2016-07-26 14:36:40 -0700

    Work on the VideoMinerDataGridView. Reworking the coloring algorithm.

--- VideoMiner/VideoMiner/frmConfigureButtons.Designer.vb
+++ VideoMiner/VideoMiner/frmConfigureButtons.Designer.vb
@@ -23,167 +23,79 @@ Partial Class frmConfigureButtons
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmConfigureButtons))
-        Me.cmdDeleteButton = New System.Windows.Forms.Button()
-        Me.cmdEditButton = New System.Windows.Forms.Button()
-        Me.cmdCreateNewButton = New System.Windows.Forms.Button()
-        Me.cmdMoveDown = New System.Windows.Forms.Button()
-        Me.cmdMoveUp = New System.Windows.Forms.Button()
         Me.cmdMoveToPanel = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.btnOK = New System.Windows.Forms.Button()
         Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnOK = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1.SuspendLayout()
-        Me.Panel1.SuspendLayout()
-        Me.Panel2.SuspendLayout()
-        Me.Panel3.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'cmdDeleteButton
-        '
-        Me.cmdDeleteButton.Dock = System.Windows.Forms.DockStyle.Left
-        Me.cmdDeleteButton.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdDeleteButton.Location = New System.Drawing.Point(118, 0)
-        Me.cmdDeleteButton.Name = "cmdDeleteButton"
-        Me.cmdDeleteButton.Size = New System.Drawing.Size(118, 44)
-        Me.cmdDeleteButton.TabIndex = 16
-        Me.cmdDeleteButton.Text = "Delete Button"
-        Me.cmdDeleteButton.UseVisualStyleBackColor = True
-        '
-        'cmdEditButton
-        '
-        Me.cmdEditButton.Dock = System.Windows.Forms.DockStyle.Left
-        Me.cmdEditButton.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdEditButton.Location = New System.Drawing.Point(0, 0)
-        Me.cmdEditButton.Name = "cmdEditButton"
-        Me.cmdEditButton.Size = New System.Drawing.Size(118, 44)
-        Me.cmdEditButton.TabIndex = 15
-        Me.cmdEditButton.Text = "Edit Button"
-        Me.cmdEditButton.UseVisualStyleBackColor = True
-        '
-        'cmdCreateNewButton
-        '
-        Me.cmdCreateNewButton.Dock = System.Windows.Forms.DockStyle.Left
-        Me.cmdCreateNewButton.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdCreateNewButton.Location = New System.Drawing.Point(236, 0)
-        Me.cmdCreateNewButton.Name = "cmdCreateNewButton"
-        Me.cmdCreateNewButton.Size = New System.Drawing.Size(118, 44)
-        Me.cmdCreateNewButton.TabIndex = 14
-        Me.cmdCreateNewButton.Text = "Add Button"
-        Me.cmdCreateNewButton.UseVisualStyleBackColor = True
-        '
-        'cmdMoveDown
-        '
-        Me.cmdMoveDown.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.cmdMoveDown.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdMoveDown.Location = New System.Drawing.Point(6, 79)
-        Me.cmdMoveDown.Name = "cmdMoveDown"
-        Me.cmdMoveDown.Size = New System.Drawing.Size(255, 23)
-        Me.cmdMoveDown.TabIndex = 13
-        Me.cmdMoveDown.Text = "Move Down"
-        Me.cmdMoveDown.UseVisualStyleBackColor = True
-        '
-        'cmdMoveUp
-        '
-        Me.cmdMoveUp.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.cmdMoveUp.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdMoveUp.Location = New System.Drawing.Point(6, 50)
-        Me.cmdMoveUp.Name = "cmdMoveUp"
-        Me.cmdMoveUp.Size = New System.Drawing.Size(255, 23)
-        Me.cmdMoveUp.TabIndex = 12
-        Me.cmdMoveUp.Text = "Move Up"
-        Me.cmdMoveUp.UseVisualStyleBackColor = True
         '
         'cmdMoveToPanel
         '
-        Me.cmdMoveToPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.cmdMoveToPanel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cmdMoveToPanel.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdMoveToPanel.Location = New System.Drawing.Point(3, 3)
+        Me.cmdMoveToPanel.Location = New System.Drawing.Point(257, 3)
         Me.cmdMoveToPanel.Name = "cmdMoveToPanel"
-        Me.cmdMoveToPanel.Size = New System.Drawing.Size(887, 44)
+        Me.cmdMoveToPanel.Size = New System.Drawing.Size(220, 38)
         Me.cmdMoveToPanel.TabIndex = 14
         Me.cmdMoveToPanel.UseVisualStyleBackColor = True
         '
         'TableLayoutPanel1
         '
-        Me.TableLayoutPanel1.ColumnCount = 2
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.cmdMoveToPanel, 0, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 1, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.Panel2, 0, 2)
-        Me.TableLayoutPanel1.Controls.Add(Me.Panel3, 0, 3)
-        Me.TableLayoutPanel1.Controls.Add(Me.Panel4, 0, 1)
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel4, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 1)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 4
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.TableLayoutPanel1.RowCount = 2
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1160, 393)
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(967, 313)
         Me.TableLayoutPanel1.TabIndex = 17
-        '
-        'Panel1
-        '
-        Me.Panel1.Controls.Add(Me.cmdMoveUp)
-        Me.Panel1.Controls.Add(Me.cmdMoveDown)
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel1.Location = New System.Drawing.Point(896, 53)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(261, 237)
-        Me.Panel1.TabIndex = 15
-        '
-        'Panel2
-        '
-        Me.Panel2.Controls.Add(Me.cmdCreateNewButton)
-        Me.Panel2.Controls.Add(Me.cmdDeleteButton)
-        Me.Panel2.Controls.Add(Me.cmdEditButton)
-        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel2.Location = New System.Drawing.Point(3, 296)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(887, 44)
-        Me.Panel2.TabIndex = 16
-        '
-        'Panel3
-        '
-        Me.TableLayoutPanel1.SetColumnSpan(Me.Panel3, 2)
-        Me.Panel3.Controls.Add(Me.btnOK)
-        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel3.Location = New System.Drawing.Point(3, 346)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(1154, 44)
-        Me.Panel3.TabIndex = 18
-        '
-        'btnOK
-        '
-        Me.btnOK.Anchor = System.Windows.Forms.AnchorStyles.None
-        Me.btnOK.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnOK.Location = New System.Drawing.Point(495, 6)
-        Me.btnOK.Name = "btnOK"
-        Me.btnOK.Size = New System.Drawing.Size(123, 38)
-        Me.btnOK.TabIndex = 19
-        Me.btnOK.Text = "DONE"
-        Me.btnOK.UseVisualStyleBackColor = True
         '
         'Panel4
         '
         Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel4.Location = New System.Drawing.Point(3, 53)
+        Me.Panel4.Location = New System.Drawing.Point(3, 3)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(887, 237)
+        Me.Panel4.Size = New System.Drawing.Size(961, 257)
         Me.Panel4.TabIndex = 19
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.cmdMoveToPanel, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnOK, 1, 0)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 266)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(961, 44)
+        Me.TableLayoutPanel2.TabIndex = 20
+        '
+        'btnOK
+        '
+        Me.btnOK.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOK.Location = New System.Drawing.Point(483, 3)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(220, 38)
+        Me.btnOK.TabIndex = 19
+        Me.btnOK.Text = "DONE"
+        Me.btnOK.UseVisualStyleBackColor = True
         '
         'frmConfigureButtons
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1160, 393)
+        Me.ClientSize = New System.Drawing.Size(967, 313)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
@@ -191,22 +103,13 @@ Partial Class frmConfigureButtons
         Me.Name = "frmConfigureButtons"
         Me.Text = "Configure Buttons"
         Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel2.ResumeLayout(False)
-        Me.Panel3.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents cmdDeleteButton As System.Windows.Forms.Button
-    Friend WithEvents cmdEditButton As System.Windows.Forms.Button
-    Friend WithEvents cmdCreateNewButton As System.Windows.Forms.Button
-    Friend WithEvents cmdMoveDown As System.Windows.Forms.Button
-    Friend WithEvents cmdMoveUp As System.Windows.Forms.Button
     Friend WithEvents cmdMoveToPanel As System.Windows.Forms.Button
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Panel2 As Panel
     Friend WithEvents btnOK As Button
-    Friend WithEvents Panel3 As Panel
     Friend WithEvents Panel4 As Panel
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
 End Class
--- VideoMiner/VideoMiner/frmConfigureButtons.vb
+++ VideoMiner/VideoMiner/frmConfigureButtons.vb
@@ -1,7 +1,6 @@
 ﻿Public Class frmConfigureButtons
 
 #Region "Member variables"
-    Private WithEvents m_frmAddButton As frmAddButton
     Private WithEvents m_grd As VideoMinerDataGridView
     ''' <summary>
     ''' The name of the table which this form represents, either
@@ -16,14 +15,6 @@
     Private m_data_code As Integer
     Private m_field_name As String
     Private m_has_modifications As Boolean
-    ''' <summary>
-    ''' Enumeration describing which way to move the button definition in the list
-    ''' </summary>
-    Private Enum MoveDirection
-        Up
-        Down
-    End Enum
-    Private m_move_direction As MoveDirection
 #End Region
 
 #Region "Events"
@@ -37,163 +28,13 @@
     Public Sub New(strConfigureTable As String)
         InitializeComponent()
         m_table_name = strConfigureTable
-        m_grd = New VideoMinerDataGridView(m_table_name, False)
+        m_grd = New VideoMinerDataGridView(m_table_name, True)
         Panel4.Controls.Add(m_grd)
         m_grd.Dock = DockStyle.Fill
     End Sub
 
     Private Sub frmConfigureButtons_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        '        populateTableList()
         m_has_modifications = False
-    End Sub
-
-    ''' <summary>
-    ''' Fill the listbox with the current m_table_name table.
-    ''' </summary>
-    Private Sub populateTableList()
-        'Dim strKeyName As String = Database.GetPrimaryKeyFieldName(m_table_name)
-        'm_data_table = Database.GetDataTable("select * from " & m_table_name & " order by " & strKeyName & " Asc", m_table_name)
-        'm_grd.DataSource = m_data_table
-        '' See m_grd_DataBindingComplete function for hiding the primary key field
-    End Sub
-
-    ''' <summary>
-    ''' Remove the Primary Key column from the view, so that users cannot change it
-    ''' </summary>
-    'Private Sub m_grd_DataBindingComplete(sender As Object, e As EventArgs) Handles m_grd.DataBindingComplete
-    ' Remove primary key field from the table which is shown
-    'Dim strPrimaryKeyName = Database.GetPrimaryKeyFieldName(m_table_name)
-    'm_grd.Columns(strPrimaryKeyName).Visible = False
-    'm_grd.Enabled = True
-    'End Sub
-
-    ''' <summary>
-    ''' Clicking this button causes the currently selected item to move up one in the list.
-    ''' The database will be modified; the primary keys for the item selected and the one above it
-    ''' will be swapped.
-    ''' </summary>
-    Private Sub cmdMoveUp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdMoveUp.Click
-        m_move_direction = MoveDirection.Up
-        moveRecord()
-    End Sub
-
-    ''' <summary>
-    ''' Clicking this button causes the currently selected item to move down one in the list.
-    ''' The database will be modified; the primary keys for the item selected and the one below it
-    ''' will be swapped.
-    ''' </summary>
-    Private Sub cmdMoveDown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdMoveDown.Click
-        m_move_direction = MoveDirection.Down
-        moveRecord()
-    End Sub
-
-    ''' <summary>
-    ''' Move the first selected item up or down in order, and raise the event to the main form so that the buttons can
-    ''' be redrawn to correspond to the new order.
-    ''' The variable m_move_direction is used to determine move direction Up or Down.
-    ''' This is accomplished by swapping the primary keys for the two records in the database.
-    ''' </summary>
-    Private Sub moveRecord()
-        Dim i As Integer
-        Dim r As DataGridViewRow
-        Dim dr As DataRow
-        Dim intSelectedIndex As Integer
-        Dim intCurrKey As Integer
-        Dim intOtherKey As Integer
-        'If m_grd.SelectedRows.Count = 0 Then
-        'MessageBox.Show("Please select a button from the table",
-        '                   "No selection", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-        'End If
-        intSelectedIndex = m_grd.FirstSelectedRowIndex
-        Dim strKeyField As String = Database.GetPrimaryKeyFieldName(m_table_name)
-        Dim intStart, intEnd, intStep As Integer
-        If m_move_direction = MoveDirection.Up Then
-            If intSelectedIndex <= 0 Then
-                ' Only move up if the item is not at the top
-                Exit Sub
-            End If
-            intStart = 0
-            intEnd = m_grd.RowCount - 1
-            intStep = 1
-            intSelectedIndex -= 1
-        Else
-            If intSelectedIndex >= m_grd.RowCount - 1 Then
-                ' Only move down if the item is not at the bottom
-                Exit Sub
-            End If
-            intStart = m_grd.RowCount - 1
-            intEnd = 0
-            intStep = -1
-            intSelectedIndex += 1
-        End If
-        ' Get the primary keys for the selected item and the one above or below it
-        For i = intStart To intEnd Step intStep
-            r = m_grd.Rows(i)
-            dr = m_data_table.Rows(r.Index)
-            If r.Selected Then
-                intCurrKey = CInt(dr.Item(strKeyField))
-                Exit For
-            Else
-                intOtherKey = CInt(dr.Item(strKeyField))
-            End If
-        Next
-        Database.SwapTwoRecords(intCurrKey, intOtherKey, m_table_name)
-        populateTableList()
-        ' Set the selected row to be the same one that has just moved
-        ' This allows user to move one item down or up quickly
-        'm_grd.ClearSelection()
-        'm_grd.Rows(intSelectedIndex).Selected = True
-        m_has_modifications = True
-    End Sub
-
-    ''' <summary>
-    ''' Clicking this button will bring up the 'add button' dialog which allows the definition of a new button.
-    ''' </summary>
-    Private Sub cmdCreateNewButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCreateNewButton.Click
-        m_frmAddButton = New frmAddButton(m_table_name)
-        m_frmAddButton.ShowDialog()
-    End Sub
-
-    ''' <summary>
-    ''' When the database has been modified by a child form, this will refresh the DataGridView
-    ''' </summary>
-    Private Sub frmAddButton_DatabaseModifiedHandler() Handles m_frmAddButton.DatabaseModifedEvent
-        populateTableList()
-        m_has_modifications = True
-    End Sub
-
-    ''' <summary>
-    ''' Clicking this button will remove the selected button from the project, including from the database.
-    ''' </summary>
-    Private Sub cmdDeleteButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDeleteButton.Click
-        Dim r As DataGridViewRow
-        Dim dr As DataRow
-        Dim intSelectedIndex As Integer
-        Dim intKey As Integer
-        If m_grd.SelectedRowCount = 0 Then
-            MessageBox.Show("Please select a button from the table",
-                            "No selection", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-        End If
-        intSelectedIndex = m_grd.FirstSelectedRowIndex
-        Dim strKeyField As String = Database.GetPrimaryKeyFieldName(m_table_name)
-        If MessageBox.Show("Are you sure you want to delete this button?",
-                           "Delete Button", MessageBoxButtons.YesNo, MessageBoxIcon.Question,
-                           MessageBoxDefaultButton.Button2) = Windows.Forms.DialogResult.No Then
-            Exit Sub
-        End If
-        ' Get the primary keys for the selected item and the one above or below it
-        For i As Integer = 0 To m_grd.RowCount - 1
-            r = m_grd.Rows(i)
-            dr = m_data_table.Rows(r.Index)
-            If r.Selected Then
-                intKey = CInt(dr.Item(strKeyField))
-                Exit For
-            End If
-        Next
-        Database.DeleteRow(intKey, m_table_name)
-        populateTableList()
-        m_grd.ClearSelection()
-        m_has_modifications = True
     End Sub
 
     ''' <summary>
@@ -230,7 +71,6 @@
         d = Database.GetDataTable("select * from " & strMoveToTable, strMoveToTable)
         Database.InsertRow(r, strMoveToTable)
         Database.DeleteRow(intLastKey, m_table_name)
-        populateTableList()
         m_has_modifications = True
     End Sub
 
@@ -238,6 +78,7 @@
         If m_has_modifications Then
             RaiseEvent DatabaseModifiedEvent()
         End If
+        m_grd.parentHiding()
         Hide()
     End Sub
 
