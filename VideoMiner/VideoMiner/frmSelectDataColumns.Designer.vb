01b1ca4aa884da65872fcd34279910c3ab93ae66
Chris Grandin <cgrandin@shaw.ca>
2016-08-02 10:30:13 -0700

    Really add the files for the previous commit to work.

---
+++ VideoMiner/VideoMiner/frmSelectDataColumns.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSelectDataColumns
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSelectDataColumns))
+        Me.clbData = New System.Windows.Forms.CheckedListBox()
+        Me.SuspendLayout()
+        '
+        'clbData
+        '
+        Me.clbData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.clbData.FormattingEnabled = True
+        Me.clbData.Location = New System.Drawing.Point(0, 0)
+        Me.clbData.Name = "clbData"
+        Me.clbData.Size = New System.Drawing.Size(269, 600)
+        Me.clbData.TabIndex = 0
+        '
+        'frmSelectDataColumns
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(269, 600)
+        Me.Controls.Add(Me.clbData)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmSelectDataColumns"
+        Me.Text = "Select visible data columns"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents clbData As CheckedListBox
+End Class
---
+++ VideoMiner/VideoMiner/frmSelectDataColumns.vb
@@ -0,0 +1,59 @@
+﻿Public Class frmSelectDataColumns
+    Private m_data_table As DataTable
+    ''' <summary>
+    ''' An array of integers holding the data table column indices that are being shown.
+    ''' </summary>
+    Private m_show_indices As Boolean()
+    Public Event DataTableModified()
+
+    ''' <summary>
+    ''' An array of booleans, 1 for each column of the data table.
+    ''' True means the column is visible, False means it is hidden.
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property VisibleColumns As Boolean()
+        Get
+            Return m_show_indices
+        End Get
+    End Property
+
+    Public Sub New(dt As DataTable)
+        InitializeComponent()
+        m_data_table = dt
+        ReDim m_show_indices(m_data_table.Columns.Count)
+        ' Add all fields found in the database table to the list
+        For col As Integer = 0 To m_data_table.Columns.Count - 1
+            clbData.Items.Add(m_data_table.Columns(col).ColumnName)
+        Next
+        checkAll()
+    End Sub
+
+    Private Sub checkAll()
+        For idx As Integer = 0 To clbData.Items.Count - 1
+            clbData.SetItemCheckState(idx, CheckState.Checked)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Record into array the visibility settings as set by user.
+    ''' </summary>
+    Private Sub recordVisibility()
+        For idx As Integer = 0 To clbData.Items.Count - 1
+            If clbData.GetItemCheckState(idx) = CheckState.Checked Then
+                m_show_indices(idx) = True
+            Else
+                m_show_indices(idx) = False
+            End If
+        Next
+    End Sub
+
+    Private Sub me_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then
+            e.Cancel = True
+            recordVisibility()
+            Hide()
+            RaiseEvent DataTableModified()
+        End If
+    End Sub
+
+End Class
